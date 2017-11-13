f5935e9e496cbff6ceaad7cef28a316fcb7b9eba
Lee Sutton <leesutton1@gmail.com>
2017-11-12 17:07:34 -0800

    Rendered template for home page view

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,7 @@ def current_file_directory():
     return os.path.dirname(
         os.path.abspath(inspect.getfile(inspect.currentframe())))
 
+
 class NewVisitorTest(unittest.TestCase):
     """
     Tests when a new user views the site
@@ -53,8 +54,23 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for
-                            row in rows), 'New to-do item not in table')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in
+            rows])
+
+        # There is still a text box inviting her to add another item
+        # she enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again and now shows both items in her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in
+            rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in
+            rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 
 
 class SmokeTest(TestCase):
+    """
+    Unit tests for the lists application
+    """
 
     def test_uses_home_template(self):
         """
@@ -13,3 +16,13 @@ class SmokeTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_can_save_POST_request(self):
+        """test_can_save_POST_request
+        Tests that the home page can save the data from
+        a post request with the input todo item
+        """
+        response = self.client.post('/',
+                                    data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
