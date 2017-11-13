b934b0893bf65db0060e961e85730e59cfcd6263
Lee Sutton <leesutton1@gmail.com>
2017-11-13 11:56:57 -0800

    Model for list Items and associated migrations

--- functional_tests.py
+++ functional_tests.py
@@ -28,6 +28,14 @@ class NewVisitorTest(unittest.TestCase):
         """
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """
+        Checks if the input row text exists in the todo list table
+        """
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -52,10 +60,8 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in
-            rows])
+        row_text = '1: Buy peacock feathers'
+        self.check_for_row_in_list_table(row_text)
 
         # There is still a text box inviting her to add another item
         # she enters "Use peacock feathers to make a fly"
@@ -65,12 +71,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again and now shows both items in her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in
-            rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in
-            rows])
+        row_text = '1: Buy peacock feathers'
+        self.check_for_row_in_list_table(row_text)
+        row_text = '2: Use peacock feathers to make a fly'
+        self.check_for_row_in_list_table(row_text)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -86,6 +90,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+
 
 
 def main():
